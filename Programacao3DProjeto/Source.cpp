b1fbaf414e17279dce90cf97052e7ac6380c3ed0
Diogo Veloso <diogo.costa.veloso@gmail.com>
2023-05-12 10:43:29 +0100

    Initial commit

---
+++ Programacao3DProjeto/Source.cpp
@@ -0,0 +1,12 @@
+#define GLEW_STATIC
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
+
+#pragma comment (lib,"glew32s.lib")
+#pragma comment (lib,"glfw3.lib")
+#pragma comment (lib,"opengl32.lib")
+
+int main(void)
+{
+	return 0;
+}
